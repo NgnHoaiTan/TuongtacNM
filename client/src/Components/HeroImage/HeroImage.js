ae37f3ba0a0761816e19d44b34b16feb8ffc0b86
Nghia Nguyen <nhn280700@gmail.com>
2022-04-07 14:02:42 +0700

    slider

--- client/src/Components/HeroImage/HeroImage.js
+++ client/src/Components/HeroImage/HeroImage.js
@@ -4,10 +4,11 @@ import { Container, Typography, Box } from '@mui/material';
 import background from "../../Images/background.jpg";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import Slider from '../Slider/Slider';
 const useStyle = makeStyles((theme) => ({
     container: {
-        marginTop: "60px",
-        marginBottom:'40px',
+        marginTop: "40px",
+        marginBottom: '40px',
         backgroundColor: "white",
         display: "flex",
         alignItems: 'center',
@@ -15,9 +16,18 @@ const useStyle = makeStyles((theme) => ({
         height: '600px',
         overflow: 'hidden',
     },
-    
+    text: {
+        flex: '1',
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center'
+    },
+    silder: {
+        flex: '1'
+    },
     introtitlemain: {
         fontSize: "60px",
+        fontWeight: '700',
         lineHeight: "120%",
     },
     introtitle: {
@@ -29,8 +39,8 @@ const HeroImage = () => {
     const classes = useStyle();
     return (
         <Container maxWidth="xl" disableGutters className={classes.container}>
-            <Box className={classes.wrapper}>
-                <div className={classes.text}>
+            <div className={classes.text}>
+                <div>
                     <Typography className={classes.introtitlemain}>
                         W I L D
                     </Typography>
@@ -41,10 +51,10 @@ const HeroImage = () => {
                         Chia sẻ và khám phá về động vật hoang dã
                     </Typography>
                 </div>
-               
-
-
-            </Box>
+            </div>
+            <div className={classes.silder}>
+                <Slider />
+            </div>
 
         </Container>
     );
